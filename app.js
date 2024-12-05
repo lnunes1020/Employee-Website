6725c7fcc873f24e91ecee99bcc93ee69766da12
ln514476 <ln514476@gsk.com>
2024-12-05 00:15:53 -0500

    setting up routes and app.js middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import exphbs  from "express-handlebars";
+import constructorMethod from "./Routes/index.js";
 const app=express();
-app.engine("handlebars", exphbs.engine({ defaultLayout: "users" }));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars")
 app.set("views", "./views");
+app.use("/public", express.static("public"));
+constructorMethod(app);
 
 app.listen(8000,()=>{
     console.log("Running on http://localhost:8000");
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,10 @@ import userRoutes from "./users.js"
 
 
 //routes
+
+const constructorMethod=(app)=>{
 console.log("getting routes");
 app.use('/users',userRoutes);
+};
+
+export default constructorMethod;
